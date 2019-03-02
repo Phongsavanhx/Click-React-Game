54d466e376223ba329fb3c6f3031d547b4f422f0
Sushi Sama <Phongsavanhx@gmail.com>
2019-03-02 13:12:02 -0700

    Updates

---
+++ precache-manifest.c2f284895d4e623db40d90ebc86f97de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Click-React-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "478c0f1c608dc1652105",
+    "url": "/Click-React-Game/static/js/runtime~main.478c0f1c.js"
+  },
+  {
+    "revision": "677beb6e8f24e65f0cfb",
+    "url": "/Click-React-Game/static/js/main.677beb6e.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Click-React-Game/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "677beb6e8f24e65f0cfb",
+    "url": "/Click-React-Game/static/css/main.4e0bf518.chunk.css"
+  },
+  {
+    "revision": "6d3a4f0832792e7132042fb611a942f8",
+    "url": "/Click-React-Game/index.html"
+  }
+];
